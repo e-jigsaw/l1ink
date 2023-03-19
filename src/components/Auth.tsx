f8c0be246b352418883d6c6039a92a7e001fb924
jigsaw <m@jgs.me>
2023-03-19 20:49:28 +0900

    use y-supabase

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,11 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
 import { Auth as SupaAuth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-
-const supabase = createClient(
-  import.meta.env.PUBLIC_SUPABASE_URL,
-  import.meta.env.PUBLIC_SUPABASE_ANON_KEY
-);
+import { supabase } from "lib/getClient";
 
 export const Auth = () => (
   <SupaAuth
