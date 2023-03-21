1bd47b70cd3d91f0a3fd2e30fe80e6d3dd9f414e
jigsaw <m@jgs.me>
2023-03-21 22:57:33 +0900

    show pages

---
+++ src/components/Project.tsx
@@ -0,0 +1,29 @@
+import { supabase } from "lib/getClient";
+import { useEffect, useState } from "react";
+
+type MiniPage = {
+  id: string;
+  title: string;
+};
+
+export const Project: React.FC<{ name: string }> = ({ name }) => {
+  const [pages, setPages] = useState<MiniPage[]>([]);
+  useEffect(() => {
+    supabase
+      .from("pages")
+      .select("title,id")
+      .eq("project", name)
+      .then((res) => {
+        if (res.data) {
+          setPages(res.data);
+        }
+      });
+  }, []);
+  return (
+    <ul>
+      {pages.map((page) => (
+        <li key={page.id}>{page.title}</li>
+      ))}
+    </ul>
+  );
+};
